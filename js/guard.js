96f5fec9467dee1717b2317867968bbb48c84e70
Rizky Pratama <104710750+Aruh1@users.noreply.github.com>
2023-10-17 17:25:48 +0000

    Add files via upload

---
+++ js/guard.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name           Anti anti-AdBlock
+// @namespace      https://github.com/igorskyflyer
+// @version        1.0.19
+// @author         Igor Dimitrijević (@igorskyflyer)
+// @description    Counters anti-AdBlock scripts
+// @homepage       https://github.com/igorskyflyer/userscript-anti-anti-adblock
+// @homepageURL    https://github.com/igorskyflyer/userscript-anti-anti-adblock
+// @website        https://igorskyflyer.me
+// @downloadURL    https://github.com/igorskyflyer/userscript-anti-anti-adblock/raw/main/anti-anti-adblock.user.js
+// @updateURL      https://github.com/igorskyflyer/userscript-anti-anti-adblock/raw/main/anti-anti-adblock.user.js
+// @supportURL     https://github.com/igorskyflyer/userscript-anti-anti-adblock/issues
+// @grant          unsafeWindow
+// @match          *://*/*
+// @run-at         document-end
+// ==/UserScript==
+
+unsafeWindow.dotcom = { flags: { adverts: true, analytics: true,  }, userinfo: { ads: true }, initAnalytics: function() {} }
+unsafeWindow.zaraz = { track: function() {} }
+unsafeWindow.demandSupply = { pfAds: true, setPfAds: function() {} }
+unsafeWindow.zarazData = {}
+unsafeWindow.AdTrack = { init: function() {} }
+unsafeWindow.MDCore = { adblock: 0 }
+
+unsafeWindow.hasAdBlocker =
+unsafeWindow.abp = false;
+
+unsafeWindow.googleAd =
+unsafeWindow.isLoadAds =
+unsafeWindow.generatorAds =
+unsafeWindow.zfgloadedpopup =
+unsafeWindow.canRunAds =
+unsafeWindow.canRunAdvertise = true;
+
+unsafeWindow.google_ad_status = 1;
+
+unsafeWindow.GeneratorAds =
+unsafeWindow.Ads_PushPage =
+unsafeWindow.Ads_Popunder =
+unsafeWindow.Ads_Vignette =
+unsafeWindow.postAntiadblockInfo =
+unsafeWindow.AdsPlugin =
+unsafeWindow.AdscoreInit = function() {}
---
+++ js/index.php
@@ -0,0 +1,80 @@
+<?php
+  if (!empty($_GET['q'])) {
+    switch ($_GET['q']) {
+      case 'info':
+        phpinfo(); 
+        exit;
+      break;
+    }
+  }
+?>
+<!DOCTYPE html>
+<html>
+<script type="text/javascript">
+    var adfly_id = 12860673;
+    var popunder_frequency_delay = 0;
+    var adfly_google_compliant = false;
+</script>
+<script src="https://cdn.adf.ly/js/display.js"></script>
+    <head>
+        <title>Yura Drive</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Karla:400" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Karla';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+
+            .opt {
+                margin-top: 30px;
+            }
+
+            .opt a {
+              text-decoration: none;
+              font-size: 150%;
+            }
+            
+            a:hover {
+              color: red;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title" title="Sy Codinger">P iyh?</div>
+     
+                <div class="opt">
+                  <div><a title="Getting Started" href="https://yurasu.xyz">Web Utama</a></div>
+				  <div><a title="Getting Started" href="https://yuradrive.xyz">Yuradrive Home</a></div>
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
